5224d09effa4c2a797d066248a1eeb6fe9dbec7c
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2024-11-07 23:24:22 -0300

    delete

--- src/hooks/useCity.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-
-import { CityContext } from '@contexts/CityContext';
-
-export function useCity() {
-  const context = useContext(CityContext)
-
-  return context
-}
